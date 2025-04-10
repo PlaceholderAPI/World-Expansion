4798d2a0e34c85acdb8853685ecf2825b2d92883
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2025-04-10 21:39:42 +0300

    Upgrade gradle, upgrade libraries, move to gradle KTS, fix recentquit not working properly, cleanup

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+}
+
+group = "me.thienbao860"
+version = "1.2.2"
+
+repositories {
+    mavenCentral()
+
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.extendedclip.com/releases/")
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:+")
+    compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
+}
+
+tasks {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+
+        withJavadocJar()
+        withSourcesJar()
+    }
+
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+}
